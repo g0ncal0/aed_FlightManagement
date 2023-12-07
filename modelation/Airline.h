5227e8e755a97dee15889280482787fe036b73f3
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2023-12-07 16:02:41 +0000

    Addded airline

--- modelation/Airline.h
+++ modelation/Airline.h
@@ -5,8 +5,37 @@
 #ifndef AVIOES_AIRLINE_H
 #define AVIOES_AIRLINE_H
 
+#include <string>
+#include <unordered_set>
 
 class Airline {
+private:
+    std::string code;
+    std::string name;
+    std::string callsign;
+    unsigned char country;
+
+public:
+    Airline(std::string code, std::string name, std::string callsign, unsigned char country){
+        this->code = code;
+        this->callsign = callsign;
+        this->callsign = callsign;
+        this->country = country;
+    }
+    std::string getCode(){return code;}
+    std::string getName(){return name;}
+    std::string getCallSign(){return callsign;}
+
+};
+
+class Airlines {
+private:
+    std::unordered_set<Airline> airlines;
+
+public:
+    void addAirline(const std::string& code, const std::string& name, const std::string& callsign, unsigned char country){
+        airlines.insert(Airline(code, name, callsign, country));
+    }
 
 };
 
